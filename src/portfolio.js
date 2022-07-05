825a76851e58b55e0b7c846d9f0586aefccee87c
lcc-jcs <60995938+lcc-jcs@users.noreply.github.com>
2022-07-05 23:45:56 +0800

    Jul052022 portfolio update

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Experienced IT professional with a demonstrated history of working in the information technology and services industry and has very strong foundation in NetSuite development."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1DZC_ok4sVfFcGzCVSY9kkPtPU2HIQEeu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1gMaAodJTrGWb4JtO09eCAC9NXbHWOfme/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
